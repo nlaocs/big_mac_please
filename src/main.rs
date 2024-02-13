09948437201c24748c09e76f5a5058d7ac7d5347
nlaocs <113911070+nlaocs@users.noreply.github.com>
2024-02-14 00:48:34 +0900

    画像の糸を読み取れていなかったため変更

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use mac_address::get_mac_address;
-
 fn main() {
-    match get_mac_address() {
-        Ok(Some(mac)) => {
-            println!("{}", mac);
-        }
-        Ok(None) => println!("(._. ))"),
-        Err(e) => println!("{:?}", e),
-    }
+    println!("FF:FF:FF:FF:FF:FF");
 }
